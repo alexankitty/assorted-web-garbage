46b20d97ff5d594467c90f96a3c3f8c324ee1d91
Alexandra <alexankitty@gmail.com>
2025-07-13 12:37:31 -0600

    add script for iis shenanigans

---
+++ iis-scrape-and-load.js
@@ -0,0 +1,68 @@
+/*
+  Intended to be ran client side. Setup a directory under IIS with directory listing enabled and pass the path into addFilesToPage
+  loadFile can be used with arbitrary content, feed it the url and mime type
+  fileTypeTest is a type map function that allows you to test a file extension via regex and assign its mime-type accordingly and then passes it through the loadFile function so it can be viewed without leaving the page.
+*/
+
+function loadFile(file, type) {
+  resourceContainer = document.createElement("div");
+  resourceBody = document.createElement("div");
+  resource = document.createElement("object");
+  title = document.createElement("p");
+  resourceClose = document.createElement("a");
+  resource.type = type;
+  resource.data = file;
+  document.body.appendChild(resourceContainer);
+  resourceContainer.appendChild(resourceClose);
+  resourceContainer.appendChild(title);
+  resourceContainer.appendChild(resourceBody);
+  title.innerHTML = file;
+  resourceClose.innerHTML = "X";
+  resourceContainer.className = "file";
+  resourceClose.className = "close";
+  title.className = "title";
+  resourceBody.className = "filebody";
+  resourceBody.appendChild(resource);
+  document.addEventListener("click", listener, true);
+  document.getElementById("overlay-back").style.display = "block";
+}
+
+function fileTypeTest(fileElement) {
+  let typeMap = {
+    "application/pdf": /.*\.pdf/,
+    "video/mp4": /.*\.mp4/,
+  };
+  for (type in typeMap) {
+    if (typeMap[type].test(fileElement.href)) {
+      fileElement.setAttribute("file", fileElement.href);
+      fileElement.onclick = function() {
+        loadFile(this.getAttribute("file"), type);
+      };
+      fileElement.href = "#";
+      return fileElement;
+    }
+  }
+  return fileElement;
+}
+
+async function addFilesToPage(file, node, name) {
+  let files = new Array();
+  const pdfCheck = /.*\.pdf/;
+  const parser = new DOMParser();
+  let html = await fetch(file).then((r) => r.text());
+  const listing = parser.parseFromString(html, "text/html");
+  const links = listing.getElementsByTagName("a");
+  for (let x = 0; x < links.length; x++) {
+    if (links[x].innerText.includes("Parent Directory")) continue;
+    files.push(fileTypeTest(links[x]));
+  }
+  if (files.length == 0) return;
+  const header = document.createElement("h2");
+  header.innerText = name;
+  nodeDOM = document.getElementById(node);
+  nodeDOM.append(header);
+  for (let x = 0; x < files.length; x++) {
+    nodeDOM.append(files[x]);
+    nodeDOM.append(document.createElement("br"));
+  }
+}
